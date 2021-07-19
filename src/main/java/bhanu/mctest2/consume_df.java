1adaef5d9aa56c6b4649bba19be406d8aea0cdcd
Bhanu Poudyal <Bhanupoudyal@hotmail.com>
2021-07-19 11:35:53 -0700

    All changes

--- src/main/java/bhanu/mctest2/consume_df.java
+++ src/main/java/bhanu/mctest2/consume_df.java
@@ -2,33 +2,38 @@ package bhanu.mctest2;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.script.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.StringWriter;
+import javax.script.ScriptException;
+import java.io.*;
 
 @SpringBootApplication
 public class consume_df {
+
     public static void main(String[] args) throws ScriptException, FileNotFoundException {
-        display_dataframe();
+        String PythonPath = "C:\\Users\\bhanu\\anaconda3\\python.exe ";
+        display_dataframe(PythonPath);
     }
 
-    public static String display_dataframe() throws FileNotFoundException, ScriptException {
-        StringWriter writer = new StringWriter();
-        ScriptContext context = new SimpleScriptContext();
-        context.setWriter(writer);
-
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("python");
-        engine.eval(new FileReader(resolvePyFilePath("mastercard_code_challenge.py")), context);
-        String dfs = writer.toString();
-        return dfs;
-
-    }
     private static String resolvePyFilePath(String filename) {
         File file = new File("src/main/resources/" + filename);
         return file.getAbsolutePath();
+    }
+
+    public static String display_dataframe(String pythonPath) throws FileNotFoundException, ScriptException {
+
+        try {
+            String WinCmd = pythonPath + resolvePyFilePath("mastercard_code_challenge.py");
+            Process p = Runtime.getRuntime().exec(WinCmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String dataFrames = reader.readLine();
+            return "<SPARKDF>" + dataFrames + "</SPARKDF>";
+//        while((s = reader.readLine())!=null){
+//            System.out.println(s);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            return new String("<FAIL>Failed</FAIL>");
+        }
+
     }
 }
 //
